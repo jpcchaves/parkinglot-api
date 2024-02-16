c40252af87de6a1f65daf16d14611ae583785227
João Chaves <joao.chaves@liferay.com>
2024-02-16 13:29:35 -0300

    initial commit

---
+++ src/main/java/com/jpcchaves/parkinglotapi/ParkinglotapiApplication.java
@@ -0,0 +1,13 @@
+package com.jpcchaves.parkinglotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotapiApplication.class, args);
+	}
+
+}
