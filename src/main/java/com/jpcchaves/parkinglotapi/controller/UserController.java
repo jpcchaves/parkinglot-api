e935fa68cc137f7fa77dd1b6901cd91f038d42da
João Chaves <joao.chaves@liferay.com>
2024-02-17 14:43:57 -0300

    feat: add create user endpoint

--- src/main/java/com/jpcchaves/parkinglotapi/controller/UserController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.jpcchaves.parkinglotapi.controller;
 
+import com.jpcchaves.parkinglotapi.domain.models.User;
 import com.jpcchaves.parkinglotapi.service.user.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,10 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
     }
 }
