f9b278252adb877db0ba2c7e347e6896e539cdec
João Chaves <joao.chaves@liferay.com>
2024-02-18 17:46:11 -0300

    feat: add user by id endpoint

--- src/main/java/com/jpcchaves/parkinglotapi/controller/UserController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/controller/UserController.java
@@ -4,10 +4,7 @@ import com.jpcchaves.parkinglotapi.domain.models.User;
 import com.jpcchaves.parkinglotapi.service.user.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -22,5 +19,10 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable(name = "userId") Long userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
     }
 }
