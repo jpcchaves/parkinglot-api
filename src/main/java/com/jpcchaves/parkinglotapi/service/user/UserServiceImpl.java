5279cf40f874e61a708565705778e00ed9d3b493
João Chaves <joao.chaves@liferay.com>
2024-02-24 19:15:35 -0300

    feat: add custom exceptions in update password service method

--- src/main/java/com/jpcchaves/parkinglotapi/service/user/UserServiceImpl.java
+++ src/main/java/com/jpcchaves/parkinglotapi/service/user/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.jpcchaves.parkinglotapi.service.user;
 
 import com.jpcchaves.parkinglotapi.domain.models.User;
 import com.jpcchaves.parkinglotapi.exception.EntityNotFoundException;
+import com.jpcchaves.parkinglotapi.exception.PasswordInvalidException;
 import com.jpcchaves.parkinglotapi.exception.UniqueConstraintViolationException;
 import com.jpcchaves.parkinglotapi.repository.UserRepository;
 import com.jpcchaves.parkinglotapi.uitls.mapper.MapperUtils;
@@ -52,17 +53,16 @@ public class UserServiceImpl implements UserService {
     public User updateUserPassword(Long userId,
                                    UserUpdatePasswordDTO requestDTO) {
         if(!passwordMatches(requestDTO.getNewPassword(), requestDTO.getConfirmNewPassword())) {
-            throw new RuntimeException("A nova senha nao condiz com a confirmacao");
+            throw new PasswordInvalidException("A nova senha nao condiz com a confirmacao");
         }
 
         User user = userRepository.findById(userId).orElseThrow(
-                () -> new RuntimeException("Usuario nao encontrado")
+                () -> new EntityNotFoundException("Usuario nao encontrado")
         );
 
         if(!passwordMatches(requestDTO.getCurrentPassword(), user.getPassword())) {
-            throw new RuntimeException("A senha atual nao condiz com a senha cadastrada pelo usuario");
+            throw new PasswordInvalidException("A senha atual nao condiz com a senha cadastrada pelo usuario");
         }
-
 
         user.setPassword(requestDTO.getNewPassword());
 
