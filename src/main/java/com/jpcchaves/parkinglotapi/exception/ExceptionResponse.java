556eed377416d22355d6f7906857252b14b63d36
João Chaves <joao.chaves@liferay.com>
2024-02-24 17:56:33 -0300

    feat: add errors attribute to display a hashmap of errors when necessary

--- src/main/java/com/jpcchaves/parkinglotapi/exception/ExceptionResponse.java
+++ src/main/java/com/jpcchaves/parkinglotapi/exception/ExceptionResponse.java
@@ -1,21 +1,34 @@
 package com.jpcchaves.parkinglotapi.exception;
 
+import org.springframework.validation.BindingResult;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ExceptionResponse {
     private Date timestamp;
     private String message;
-    private String details;
     private String path;
+    private Map<String, String> errors;
 
     public ExceptionResponse(Date timestamp,
                              String message,
-                             String details,
                              String path) {
         this.timestamp = timestamp;
         this.message = message;
-        this.details = details;
         this.path = path;
+    }
+
+    public ExceptionResponse(Date timestamp,
+                             String message,
+                             String path,
+                             BindingResult result
+    ) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.path = path;
+        addErrors(result);
     }
 
     public Date getTimestamp() {
@@ -34,19 +47,28 @@ public class ExceptionResponse {
         this.message = message;
     }
 
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
     public String getPath() {
         return path;
     }
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+
+    private void addErrors(BindingResult result) {
+        this.errors = new HashMap<>();
+
+        result.getFieldErrors()
+                .forEach(fieldError -> {
+                    this.errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+                });
     }
 }
