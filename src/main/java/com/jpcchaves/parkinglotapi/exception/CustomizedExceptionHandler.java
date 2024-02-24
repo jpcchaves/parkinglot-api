6ead13775bd0a07afb5b58648116125238b2e5b2
João Chaves <joao.chaves@liferay.com>
2024-02-24 18:12:53 -0300

    feat: add handler for method argument not valid exception

--- src/main/java/com/jpcchaves/parkinglotapi/exception/CustomizedExceptionHandler.java
+++ src/main/java/com/jpcchaves/parkinglotapi/exception/CustomizedExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.jpcchaves.parkinglotapi.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
@@ -21,5 +24,17 @@ public class CustomizedExceptionHandler {
                 request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public final ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                                         HttpServletRequest request,
+                                                                                         BindingResult result) {
+        logger.severe("Error: " + ex.getClass() + " Message: " + ex.getMessage());
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Campos invalidos!",
+                request.getRequestURI(), result);
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
