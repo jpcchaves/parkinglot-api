192cfee00b066b65c61b8c66b773bb8bce4dfac4
João Chaves <joao.chaves@liferay.com>
2024-02-25 12:00:33 -0300

    docs: add docs for user resources

--- src/main/java/com/jpcchaves/parkinglotapi/web/controller/UserController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/web/controller/UserController.java
@@ -6,6 +6,7 @@ import com.jpcchaves.parkinglotapi.web.dto.user.UserResponseDTO;
 import com.jpcchaves.parkinglotapi.web.dto.user.UserUpdatePasswordDTO;
 import com.jpcchaves.parkinglotapi.web.exception.ExceptionResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -66,17 +67,90 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(requestDTO));
     }
 
+    @Operation(
+            summary = "Find user by id",
+            description = "Find a new user by passing its ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Gets the user by id",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = UserResponseDTO.class
+                                    ))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+            }
+    )
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable(name = "userId") Long userId) {
         return ResponseEntity.ok(userService.getUserById(userId));
     }
 
+    @Operation(
+            summary = "Updates user password",
+            description = "Updates user password",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Updates user password",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema()
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+            }
+    )
     @PatchMapping("/{userId}")
     public ResponseEntity<Void> updateUserPassword(@PathVariable(name = "userId") Long userId, @Valid @RequestBody UserUpdatePasswordDTO requestDTO) {
         userService.updateUserPassword(userId, requestDTO);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Gets the users list",
+            description = "Gets the users list",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Gets the users list",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(
+                                            schema = @Schema(
+                                                    implementation = UserResponseDTO.class
+                                            )
+                                    ))
+                    ),
+            }
+    )
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> listAllUsers() {
         return ResponseEntity.ok(userService.listAllUsers());
