ee453c5c69b1b5ac1f253e4bcc5c8dde3f084316
João Chaves <joao.chaves@liferay.com>
2024-02-25 11:52:06 -0300

    docs: add documentation for user create endpoint

--- src/main/java/com/jpcchaves/parkinglotapi/web/controller/UserController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/web/controller/UserController.java
@@ -4,6 +4,12 @@ import com.jpcchaves.parkinglotapi.service.user.UserService;
 import com.jpcchaves.parkinglotapi.web.dto.user.UserCreateDTO;
 import com.jpcchaves.parkinglotapi.web.dto.user.UserResponseDTO;
 import com.jpcchaves.parkinglotapi.web.dto.user.UserUpdatePasswordDTO;
+import com.jpcchaves.parkinglotapi.web.exception.ExceptionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Users", description = "Controller to manage users")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
@@ -21,6 +28,39 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Creates a new user",
+            description = "Creates a new user by passing its JSON representation",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Usuario criado com sucesso",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = UserResponseDTO.class
+                                    ))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+            }
+    )
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserCreateDTO requestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(requestDTO));
