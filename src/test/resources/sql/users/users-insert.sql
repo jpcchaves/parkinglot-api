c46cfaaab1a12aeb007b1dd9242a4fa0b03454c1
João Chaves <joao.chaves@liferay.com>
2024-02-26 21:42:10 -0300

    feat: add users insert sql to seed h2 db

---
+++ src/test/resources/sql/users/users-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO USERS (username, password, role) VALUES
+                                                 ("test@test.com", "123456", "ROLE_ADMIN"),
+                                                 ("test2@test.com", "123456", "ROLE_ADMIN"),
+                                                 ("test3@test.com", "123456", "ROLE_ADMIN"),
+                                                 ("test4@test.com", "123456", "ROLE_ADMIN"),
+                                                 ("test5@test.com", "123456", "ROLE_ADMIN"),
+                                                 ("test6@test.com", "123456", "ROLE_CLIENT"),
+                                                 ("test7@test.com", "123456", "ROLE_CLIENT");
