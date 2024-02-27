ab7f22c521021f504eb2c7e4c463b675611f7ec3
João Chaves <joao.chaves@liferay.com>
2024-02-26 21:53:08 -0300

    feat: add user IT

--- src/test/java/com/jpcchaves/parkinglotapi/UserIT.java
+++ src/test/java/com/jpcchaves/parkinglotapi/UserIT.java
@@ -1,7 +1,12 @@
 package com.jpcchaves.parkinglotapi;
 
+import com.jpcchaves.parkinglotapi.web.dto.user.UserCreateDTO;
+import com.jpcchaves.parkinglotapi.web.dto.user.UserResponseDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -19,4 +24,23 @@ public class UserIT {
     @Autowired
     WebTestClient testClient;
 
+    @Test
+    public void createUsuario() {
+        UserResponseDTO response = testClient
+                .post()
+                .uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDTO("testeIT@email.com", "123456"))
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(UserResponseDTO.class)
+                .returnResult()
+                .getResponseBody();
+
+
+        assert response != null;
+        Assertions.assertEquals(response.getUsername(), "testeIT@email.com");
+        Assertions.assertEquals(response.getRole(), "CLIENT");
+    }
 }
