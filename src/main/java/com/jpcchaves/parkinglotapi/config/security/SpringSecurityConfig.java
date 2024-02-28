7a66dec0c5892a21d8ddc85a1ae121f3e8a35b64
João Chaves <joao.chaves@liferay.com>
2024-02-27 21:41:13 -0300

    feat: add password encoder bean

--- src/main/java/com/jpcchaves/parkinglotapi/config/security/SpringSecurityConfig.java
+++ src/main/java/com/jpcchaves/parkinglotapi/config/security/SpringSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -32,5 +34,10 @@ public class SpringSecurityConfig {
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
