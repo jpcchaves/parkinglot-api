81a815cf77a3d4b191f246e66549bb36febef67f
João Chaves <joao.chaves@liferay.com>
2024-02-27 21:42:13 -0300

    feat: add authentication manager bean

--- src/main/java/com/jpcchaves/parkinglotapi/config/security/SpringSecurityConfig.java
+++ src/main/java/com/jpcchaves/parkinglotapi/config/security/SpringSecurityConfig.java
@@ -3,6 +3,8 @@ package com.jpcchaves.parkinglotapi.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -39,5 +41,11 @@ public class SpringSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration
+                .getAuthenticationManager();
     }
 }
