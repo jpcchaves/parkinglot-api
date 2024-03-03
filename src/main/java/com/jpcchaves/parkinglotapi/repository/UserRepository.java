0bac4308922ae8abf65ea2db78a8a709fbf1c4ab
João Chaves <joao.chaves@liferay.com>
2024-03-02 21:37:21 -0300

    feat: add findByUsername and findRoleByUsername jpa methods

--- src/main/java/com/jpcchaves/parkinglotapi/repository/UserRepository.java
+++ src/main/java/com/jpcchaves/parkinglotapi/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package com.jpcchaves.parkinglotapi.repository;
 
+import com.jpcchaves.parkinglotapi.domain.Enum.Role;
 import com.jpcchaves.parkinglotapi.domain.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    @Query(
+            "select u.role from User where u.username like :username"
+    )
+    Role findRoleByUsername(String username);
 }
