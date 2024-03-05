092ab71b10d1db3b4cfc6d01d2082ebd9d035c75
João Chaves <joao.chaves@liferay.com>
2024-03-04 22:32:18 -0300

    fix: adjust jwt user details service to use the jpa interface instead of the service class

--- src/main/java/com/jpcchaves/parkinglotapi/jwt/JwtUserDetailsService.java
+++ src/main/java/com/jpcchaves/parkinglotapi/jwt/JwtUserDetailsService.java
@@ -2,7 +2,8 @@ package com.jpcchaves.parkinglotapi.jwt;
 
 import com.jpcchaves.parkinglotapi.domain.Enum.Role;
 import com.jpcchaves.parkinglotapi.domain.models.User;
-import com.jpcchaves.parkinglotapi.service.user.UserService;
+import com.jpcchaves.parkinglotapi.exception.EntityNotFoundException;
+import com.jpcchaves.parkinglotapi.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,20 +11,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
-    private final UserService userService;
+    private final UserRepository userRepository;
 
-    public JwtUserDetailsService(UserService userService) {
-        this.userService = userService;
+    public JwtUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.getUserByUsername(username);
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new EntityNotFoundException("Usuario nao encontrado com o email informado %s".formatted(username)));
         return new JwtUserDetails(user);
     }
 
     public JwtToken getTokenAuthenticated(String username) {
-        Role role = userService.findRoleByUsername(username);
+        Role role = userRepository.findRoleByUsername(username);
         return JwtUtils.createToken(username, role.name().substring("ROLE_".length()));
     }
 }
