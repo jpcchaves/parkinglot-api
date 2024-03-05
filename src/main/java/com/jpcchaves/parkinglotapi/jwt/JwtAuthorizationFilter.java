ec07d5e7ad0ed496e488f968c39d623ea531dd4f
João Chaves <joao.chaves@liferay.com>
2024-03-04 22:33:26 -0300

    refactor: adjust jwt auth filter

--- src/main/java/com/jpcchaves/parkinglotapi/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/jpcchaves/parkinglotapi/jwt/JwtAuthorizationFilter.java
@@ -4,19 +4,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
+@Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtUserDetailsService jwtUserDetailsService;
-    private static final Logger logger = Logger.getLogger(JwtAuthorizationFilter.class.getName());
 
     public JwtAuthorizationFilter(JwtUserDetailsService jwtUserDetailsService) {
         this.jwtUserDetailsService = jwtUserDetailsService;
@@ -26,38 +28,40 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION);
+        String token = getTokenFromRequest(request);
 
-        if(token == null || token.startsWith(JwtUtils.JWT_BEARER)){
-            logger.info("JWT esta nulo, vazio, ou tem um formato invalido!");
-            filterChain.doFilter(request, response);
-            return;
+        if (isTokenValid(token)) {
+            String username = JwtUtils.getUsernameFromToken(token);
+            UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities()
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
-
-        if (!JwtUtils.isTokenValid(token)) {
-            logger.warning("JWT invalido ou expirado!");
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        String username = JwtUtils.getUsernameFromToken(token);
-
-        toAuthentication(request, username);
 
         filterChain.doFilter(request, response);
+
     }
 
-    private void toAuthentication(HttpServletRequest request, String username) {
-        UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (checkIfHeaderHasBearerToken(token)) {
+            return token.substring(7);
+        }
+        return null;
+    }
 
-        UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken
-                .authenticated(userDetails, null, userDetails.getAuthorities());
+    private Boolean checkIfHeaderHasBearerToken(String token) {
+        return StringUtils.hasText(token) && token.startsWith("Bearer ");
+    }
 
-        authenticationToken.setDetails(
-                new WebAuthenticationDetailsSource()
-                        .buildDetails(request)
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    private Boolean isTokenValid(String token) {
+        return StringUtils.hasText(token) && JwtUtils.isTokenValid(token);
     }
 }
