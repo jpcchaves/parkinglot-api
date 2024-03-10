8c594e93809fc4d9cc904f37170a0e84799319e5
João Chaves <joao.chaves@liferay.com>
2024-03-10 09:49:56 -0300

    docs: add doc for login endpoint

--- src/main/java/com/jpcchaves/parkinglotapi/web/controller/AuthController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/web/controller/AuthController.java
@@ -3,7 +3,13 @@ package com.jpcchaves.parkinglotapi.web.controller;
 import com.jpcchaves.parkinglotapi.jwt.JwtToken;
 import com.jpcchaves.parkinglotapi.jwt.JwtUserDetailsService;
 import com.jpcchaves.parkinglotapi.web.dto.user.UserLoginDTO;
+import com.jpcchaves.parkinglotapi.web.dto.user.UserResponseDTO;
 import com.jpcchaves.parkinglotapi.web.exception.ExceptionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 import java.util.logging.Logger;
 
+@Tag(name = "Authetication", description = "Resource to allow users to deal with authentication in the API")
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
@@ -31,6 +38,39 @@ public class AuthController {
         this.authenticationManager = authenticationManager;
     }
 
+    @Operation(
+            summary = "Authenticates a user",
+            description = "Authenticates a user by passing an email and password",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User authenticated successfully!",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = UserResponseDTO.class
+                                    ))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+            }
+    )
     @PostMapping(path = {"/signin", "/login"})
     public ResponseEntity<?> login(@Valid @RequestBody UserLoginDTO requestDTO, HttpServletRequest httpServletRequest) {
         try {
