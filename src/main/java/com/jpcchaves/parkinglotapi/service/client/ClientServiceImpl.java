14cbde25c05db50caa008258888bc86e1df90a1e
João Chaves <joao.chaves@liferay.com>
2024-03-12 20:30:57 -0300

    feat: adjust create client service method create the relation between the user and the client

--- src/main/java/com/jpcchaves/parkinglotapi/service/client/ClientServiceImpl.java
+++ src/main/java/com/jpcchaves/parkinglotapi/service/client/ClientServiceImpl.java
@@ -1,12 +1,15 @@
 package com.jpcchaves.parkinglotapi.service.client;
 
 import com.jpcchaves.parkinglotapi.domain.models.Client;
+import com.jpcchaves.parkinglotapi.domain.models.User;
 import com.jpcchaves.parkinglotapi.exception.CpfUniqueViolationException;
 import com.jpcchaves.parkinglotapi.repository.ClientRepository;
+import com.jpcchaves.parkinglotapi.service.user.UserService;
 import com.jpcchaves.parkinglotapi.uitls.mapper.MapperUtils;
 import com.jpcchaves.parkinglotapi.web.dto.client.ClientCreateDTO;
 import com.jpcchaves.parkinglotapi.web.dto.client.ClientResponseDTO;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,18 +17,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class ClientServiceImpl implements ClientService {
     private final ClientRepository clientRepository;
     private final MapperUtils mapperUtils;
+    private final UserService userService;
 
     public ClientServiceImpl(ClientRepository clientRepository,
-                             MapperUtils mapperUtils) {
+                             MapperUtils mapperUtils,
+                             UserService userService) {
         this.clientRepository = clientRepository;
         this.mapperUtils = mapperUtils;
+        this.userService = userService;
     }
 
     @Override
     @Transactional
     public ClientResponseDTO create(ClientCreateDTO requestDTO) {
         try {
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            userService.getUserById(user.getId());
             Client client = mapperUtils.parseObject(requestDTO, Client.class);
+
             Client savedClient = clientRepository.save(client);
             return mapperUtils.parseObject(savedClient, ClientResponseDTO.class);
         } catch (DataIntegrityViolationException ex) {
