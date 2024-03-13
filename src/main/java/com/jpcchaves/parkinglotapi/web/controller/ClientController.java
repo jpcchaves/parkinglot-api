27e8ef1d40a23c3ebb936e7b6d8a53a650cba38a
João Chaves <joao.chaves@liferay.com>
2024-03-12 21:05:34 -0300

    feat: add get client by id endpoint

--- src/main/java/com/jpcchaves/parkinglotapi/web/controller/ClientController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/web/controller/ClientController.java
@@ -3,19 +3,18 @@ package com.jpcchaves.parkinglotapi.web.controller;
 import com.jpcchaves.parkinglotapi.service.client.ClientService;
 import com.jpcchaves.parkinglotapi.web.dto.client.ClientCreateDTO;
 import com.jpcchaves.parkinglotapi.web.dto.client.ClientResponseDTO;
+import com.jpcchaves.parkinglotapi.web.dto.user.UserResponseDTO;
 import com.jpcchaves.parkinglotapi.web.exception.ExceptionResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/clients")
@@ -27,6 +26,7 @@ public class ClientController {
     }
 
     @Operation(
+            security = @SecurityRequirement(name = "security"),
             summary = "Creates a client",
             description = "Creates a new client related to an user",
             responses = {
@@ -63,5 +63,36 @@ public class ClientController {
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<ClientResponseDTO> create(@Valid @RequestBody ClientCreateDTO requestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(clientService.create(requestDTO));
+    }
+
+
+    @Operation(
+            security = @SecurityRequirement(name = "security"),
+            summary = "Find client by id",
+            description = "Find a client by passing its ID",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Gets the client by id",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = UserResponseDTO.class
+                                    ))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ExceptionResponse.class
+                                    ))
+                    ),
+            }
+    )
+    @GetMapping("/{clientId}")
+    public ResponseEntity<ClientResponseDTO> getById(@PathVariable(name = "clientId") Long clientId) {
+        return ResponseEntity.ok(clientService.getById(clientId));
     }
 }
