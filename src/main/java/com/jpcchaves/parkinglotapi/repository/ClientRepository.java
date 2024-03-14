738196b31f3c4d23f9920a83474761dc978aca7f
João Chaves <joao.chaves@liferay.com>
2024-03-14 19:36:48 -0300

    feat: add jpql jpa repository method to find all pageable

--- src/main/java/com/jpcchaves/parkinglotapi/repository/ClientRepository.java
+++ src/main/java/com/jpcchaves/parkinglotapi/repository/ClientRepository.java
@@ -1,9 +1,16 @@
 package com.jpcchaves.parkinglotapi.repository;
 
 import com.jpcchaves.parkinglotapi.domain.models.Client;
+import com.jpcchaves.parkinglotapi.repository.projection.ClientProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    @Query("select c from Client c")
+    Page<ClientProjection> findAllPageable(Pageable pageable);
 }
