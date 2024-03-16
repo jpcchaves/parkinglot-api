28a0875107fddd3bd8d94177550c1901549cec7a
João Chaves <joao.chaves@liferay.com>
2024-03-16 12:51:50 -0300

    feat: add get by code endpoint

--- src/main/java/com/jpcchaves/parkinglotapi/web/controller/ParkingSpaceController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/web/controller/ParkingSpaceController.java
@@ -2,15 +2,12 @@ package com.jpcchaves.parkinglotapi.web.controller;
 
 import com.jpcchaves.parkinglotapi.service.parkingspace.ParkingSpaceService;
 import com.jpcchaves.parkinglotapi.web.dto.parkingspace.ParkingSpaceCreateDTO;
-import com.jpcchaves.parkinglotapi.web.dto.parkingspace.ParkingSpaceRespondeDTO;
+import com.jpcchaves.parkinglotapi.web.dto.parkingspace.ParkingSpaceResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/parking-spaces")
@@ -23,9 +20,15 @@ public class ParkingSpaceController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<ParkingSpaceRespondeDTO> create(@RequestBody @Valid
+    public ResponseEntity<ParkingSpaceResponseDTO> create(@RequestBody @Valid
                                                           ParkingSpaceCreateDTO requestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpaceService.create(requestDTO));
+    }
+
+    @GetMapping("/{code}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ParkingSpaceResponseDTO> getByCode(@PathVariable(name = "code") String code) {
+        return ResponseEntity.ok(parkingSpaceService.getByCode(code));
     }
 
 }
