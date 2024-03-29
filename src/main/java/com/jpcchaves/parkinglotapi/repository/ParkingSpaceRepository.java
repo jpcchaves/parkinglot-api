94d1059f1347721c42414a93647529c9ab016906
João Chaves <joao.chaves@liferay.com>
2024-03-29 16:36:46 -0300

    feat: add jpa method to find the first parking space by a given status

--- src/main/java/com/jpcchaves/parkinglotapi/repository/ParkingSpaceRepository.java
+++ src/main/java/com/jpcchaves/parkinglotapi/repository/ParkingSpaceRepository.java
@@ -1,5 +1,6 @@
 package com.jpcchaves.parkinglotapi.repository;
 
+import com.jpcchaves.parkinglotapi.domain.Enum.ParkingSpaceStatus;
 import com.jpcchaves.parkinglotapi.domain.models.ParkingSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace, Long> {
-    Optional<ParkingSpace> findByParkingSpaceCode(String code);
+  Optional<ParkingSpace> findByParkingSpaceCode(String code);
+
+  Optional<ParkingSpace> findFirstByStatus(ParkingSpaceStatus parkingSpaceStatus);
 }
