9bfbc3328f2c41ecef75050ee4148d0892f59bc6
João Chaves <joao.chaves@liferay.com>
2024-03-29 16:37:23 -0300

    feat: add method to do a check in when someone parks in the parking lot

--- src/main/java/com/jpcchaves/parkinglotapi/service/client/ParkingLotServiceImpl.java
+++ src/main/java/com/jpcchaves/parkinglotapi/service/client/ParkingLotServiceImpl.java
@@ -1,21 +1,59 @@
 package com.jpcchaves.parkinglotapi.service.client;
 
+import com.jpcchaves.parkinglotapi.domain.Enum.ParkingSpaceStatus;
+import com.jpcchaves.parkinglotapi.domain.models.Client;
+import com.jpcchaves.parkinglotapi.domain.models.ClientParkingSpace;
+import com.jpcchaves.parkinglotapi.domain.models.ParkingSpace;
+import com.jpcchaves.parkinglotapi.exception.EntityNotFoundException;
+import com.jpcchaves.parkinglotapi.exception.NoParkingSpaceAvailableExeception;
 import com.jpcchaves.parkinglotapi.repository.ClientParkingSpaceRepository;
 import com.jpcchaves.parkinglotapi.repository.ClientRepository;
 import com.jpcchaves.parkinglotapi.repository.ParkingSpaceRepository;
-import org.springframework.stereotype.Component;
+import com.jpcchaves.parkinglotapi.util.ParkingSpaceUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Component
+import java.time.LocalDateTime;
+
+@Service
 public class ParkingLotServiceImpl implements ParkingLotService {
-    private final ClientParkingSpaceRepository clientParkingSpaceRepository;
-    private final ClientRepository clientRepository;
-    private final ParkingSpaceRepository parkingSpaceRepository;
+  private final ClientParkingSpaceRepository clientParkingSpaceRepository;
+  private final ClientRepository clientRepository;
+  private final ParkingSpaceRepository parkingSpaceRepository;
 
-    public ParkingLotServiceImpl(ClientParkingSpaceRepository clientParkingSpaceRepository,
-                                 ClientRepository clientRepository,
-                                 ParkingSpaceRepository parkingSpaceRepository) {
-        this.clientParkingSpaceRepository = clientParkingSpaceRepository;
-        this.clientRepository = clientRepository;
-        this.parkingSpaceRepository = parkingSpaceRepository;
-    }
+  public ParkingLotServiceImpl(ClientParkingSpaceRepository clientParkingSpaceRepository,
+                               ClientRepository clientRepository,
+                               ParkingSpaceRepository parkingSpaceRepository) {
+    this.clientParkingSpaceRepository = clientParkingSpaceRepository;
+    this.clientRepository = clientRepository;
+    this.parkingSpaceRepository = parkingSpaceRepository;
+  }
+
+  @Transactional
+  public ClientParkingSpace checkIn(ClientParkingSpace clientParkingSpace) {
+    Client client = getClientByCpf(clientParkingSpace.getClient().getCpf());
+    clientParkingSpace.setClient(client);
+
+    ParkingSpace parkingSpace = findFreeParkingSpace();
+    parkingSpace.setStatus(ParkingSpaceStatus.OCCUPIED);
+
+    clientParkingSpace.setEntryDate(LocalDateTime.now());
+    clientParkingSpace.setReceipt(ParkingSpaceUtils.generateReceipt());
+
+    return clientParkingSpaceRepository.save(clientParkingSpace);
+  }
+
+  @Transactional(readOnly = true)
+  public Client getClientByCpf(String cpf) {
+    return clientRepository
+        .findClientByCpf(cpf)
+        .orElseThrow(() -> new EntityNotFoundException("No client found with the given CPF"));
+  }
+
+  @Transactional(readOnly = true)
+  public ParkingSpace findFreeParkingSpace() {
+    return parkingSpaceRepository
+        .findFirstByStatus(ParkingSpaceStatus.AVAILABLE)
+        .orElseThrow(() -> new NoParkingSpaceAvailableExeception("There is no parking space available"));
+  }
 }
