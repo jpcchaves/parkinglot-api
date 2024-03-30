309e9fd74a06d275b46268f7d9908b2ba2e39d66
João Chaves <joao.chaves@liferay.com>
2024-03-30 16:46:03 -0300

    docs: add documentation for checkin endpoint

--- src/main/java/com/jpcchaves/parkinglotapi/web/controller/ParkingLotController.java
+++ src/main/java/com/jpcchaves/parkinglotapi/web/controller/ParkingLotController.java
@@ -4,6 +4,9 @@ package com.jpcchaves.parkinglotapi.web.controller;
 import com.jpcchaves.parkinglotapi.service.client.ParkingLotService;
 import com.jpcchaves.parkinglotapi.web.dto.parkingspace.ParkingCreateDTO;
 import com.jpcchaves.parkinglotapi.web.dto.parkingspace.ParkingResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,6 +24,15 @@ public class ParkingLotController {
     this.parkingLotService = parkingLotService;
   }
 
+  @Operation(
+      summary = "Check in of a car",
+      description = "Check in when a car enters the parking lot",
+      security = @SecurityRequirement(name = "security"),
+      responses = {
+          @ApiResponse(responseCode = "404", description = "Bad request")
+      }
+
+  )
   @PostMapping("/check-in")
   @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<ParkingResponseDTO> checkIn(@RequestBody @Valid
