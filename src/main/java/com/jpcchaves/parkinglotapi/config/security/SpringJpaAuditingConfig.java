3b2ac1161f788610f91dcfcdca7c48b6f30a7fc5
João Chaves <joao.chaves@liferay.com>
2024-03-30 20:10:06 -0300

    refactor: adjust warning about not null api

--- src/main/java/com/jpcchaves/parkinglotapi/config/security/SpringJpaAuditingConfig.java
+++ src/main/java/com/jpcchaves/parkinglotapi/config/security/SpringJpaAuditingConfig.java
@@ -6,17 +6,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import reactor.util.annotation.NonNull;
 
 import java.util.Optional;
 
 @EnableJpaAuditing
 @Configuration
 public class SpringJpaAuditingConfig implements AuditorAware<String> {
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        return Optional.ofNullable(SecurityContextHolder.getContext())
-                .map(SecurityContext::getAuthentication)
-                .filter(Authentication::isAuthenticated)
-                .map(Authentication::getName);
-    }
+  @Override
+  @NonNull
+  public Optional<String> getCurrentAuditor() {
+    return Optional.ofNullable(SecurityContextHolder.getContext())
+        .map(SecurityContext::getAuthentication)
+        .filter(Authentication::isAuthenticated)
+        .map(Authentication::getName);
+  }
 }
