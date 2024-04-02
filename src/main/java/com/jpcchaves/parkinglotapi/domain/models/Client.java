8f269f9e27b06c02785d31ed89c08e4ceed77a46
João Chaves <joao.chaves@liferay.com>
2024-04-01 22:51:41 -0300

    refactor: add audited entity inherit in client entity

--- src/main/java/com/jpcchaves/parkinglotapi/domain/models/Client.java
+++ src/main/java/com/jpcchaves/parkinglotapi/domain/models/Client.java
@@ -1,10 +1,6 @@
 package com.jpcchaves.parkinglotapi.domain.models;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serial;
@@ -15,141 +11,101 @@ import java.util.Objects;
 @Entity
 @Table(name = "clients")
 @EntityListeners(AuditingEntityListener.class)
-public class Client implements Serializable {
-    @Serial
-    private static final long serialVersionUID = -4727968164755556185L;
+public class Client extends AuditedEntity implements Serializable {
+  @Serial
+  private static final long serialVersionUID = -4727968164755556185L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "id", nullable = false)
+  private Long id;
 
-    @Column(length = 100, nullable = false)
-    private String name;
+  @Column(length = 100, nullable = false)
+  private String name;
 
-    @Column(nullable = false, unique = true, length = 11)
-    private String cpf;
+  @Column(nullable = false, unique = true, length = 11)
+  private String cpf;
 
-    @OneToOne(
-            fetch = FetchType.LAZY
-    )
-    @JoinColumn(
-            name = "user_id",
-            nullable = false
-    )
-    private User user;
+  @OneToOne(
+      fetch = FetchType.LAZY
+  )
+  @JoinColumn(
+      name = "user_id",
+      nullable = false
+  )
+  private User user;
 
-    @CreatedDate
-    @Column
-    private LocalDateTime createdAt;
 
-    @LastModifiedDate
-    @Column
-    private LocalDateTime updatedAt;
+  public Client() {
+  }
 
-    @CreatedBy
-    @Column
-    private String createdBy;
+  public Client(LocalDateTime createdAt,
+                LocalDateTime updatedAt,
+                String createdBy,
+                String modifiedBy,
+                Long id,
+                String name,
+                String cpf,
+                User user) {
+    super(createdAt, updatedAt, createdBy, modifiedBy);
+    this.id = id;
+    this.name = name;
+    this.cpf = cpf;
+    this.user = user;
+  }
 
-    @LastModifiedBy
-    @Column
-    private String modifiedBy;
+  public Long getId() {
+    return id;
+  }
 
-    public Client() {
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public Client(Long id,
-                  String name,
-                  String cpf,
-                  User user,
-                  LocalDateTime createdAt,
-                  LocalDateTime updatedAt,
-                  String createdBy,
-                  String modifiedBy) {
-        this.id = id;
-        this.name = name;
-        this.cpf = cpf;
-        this.user = user;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.createdBy = createdBy;
-        this.modifiedBy = modifiedBy;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public String getCpf() {
+    return cpf;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public User getUser() {
+    return user;
+  }
 
-    public String getCpf() {
-        return cpf;
-    }
+  public void setUser(User user) {
+    this.user = user;
+  }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Client client = (Client) o;
+    return Objects.equals(id, client.id);
+  }
 
-    public User getUser() {
-        return user;
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return Objects.equals(id, client.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  @Override
+  public String toString() {
+    return "Client{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", cpf='" + cpf + '\'' +
+        ", user=" + user +
+        '}';
+  }
 }
